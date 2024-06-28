4a1d6f6a25c08aad20b9a7efe416837584b422a4
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-06-28 12:58:00 +0530

    setup

---
+++ src/test/java/com/app/attendance/AttendanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
